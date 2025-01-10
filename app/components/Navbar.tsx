ee91d68d827d2752f4e8cf4e3d617740fb30d8d1
Gim128 <gimhanpabasara4@gmail.com>
2025-01-11 01:11:31 +0530

    navbar created

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useSession } from 'next-auth/react';
+import { useSession, signIn, signOut } from 'next-auth/react';
 import Link from 'next/link';
 
 const Navbar = () => {
@@ -6,9 +6,39 @@ const Navbar = () => {
 
     return (
         <nav className='bg-blue-500 p-4'>
-            <div >
+            <div className="max-w-7xl mx-auto flex justify-between items-center">
+                <div className="text-white text-xl">
+                    <Link href='/'>GSPro</Link>
+                </div>
+
+                <div className="flex space-x-4">
+                    <Link href='/'>Home</Link>
+                    <Link href='/about'>About</Link>
+                    <Link href='/services'>Services</Link>
+                    <Link href='/stores'>Stores</Link>
+
+                    {!session ? (
+                        <>
+                        <button onClick={() => signIn()} className="text-white">
+                            Login
+                        </button>
+
+                        <Link href="/auth/signup">
+                            <button className="text-white">Sign Up</button>
+                        </Link>
+                        </>
+                    ) : (
+                        <button onClick={() => signOut()} className="text-white">
+                            Logout
+                        </button>
+                    )
+
+                    }
+                </div>
 
             </div>
         </nav>
     )
 }
+
+export default Navbar
