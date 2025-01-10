88826842f88ef7a833a629c55ff4c8a02a6033f5
Gim128 <gimhanpabasara4@gmail.com>
2025-01-11 01:34:09 +0530

    login.tsx created

--- app/pages/auth/login.tsx
+++ app/pages/auth/login.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import {signIn} from 'next-auth/react'
+import Google from 'next-auth/providers/google'
 
 const login = () => {
   return (
-    <div>login</div>
+    <div className="max-w-md mx-auto p-4">
+      <h2 className="text-2xl font-bold">Login Here</h2>
+      <div className='mt-4'>
+        <button className="w-full bg-blue-500 text-white py-2 rounded"
+         onClick={() => signIn('google')}>
+          Login with Google
+        </button>
+      </div>
+      <div className="mt-4">
+        <p>Or log in with email :</p>
+        {/*Your email login here*/}
+      </div>
+    </div>
   )
 }
 
